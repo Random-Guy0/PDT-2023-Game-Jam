61f83cde30bd81506d41eec19cb606038d870632
Zacosk <62122304+Zacosk@users.noreply.github.com>
2023-07-30 23:15:34 +1000

    Added basic abilities
    - Added double jump and speed boost abilites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,29 +6,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public enum Abilities
+    {
+        Default,
+        DoubleJump,
+        SpeedBoost
+    }
+
+    [SerializeField] private Abilities activeAbility;
+    
     public CharacterController controller;
 
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float jumpHeight = 10f;
+    [SerializeField] private float speedMultiplier = 20f;
 
     [SerializeField] private Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    private int jumpCount = 0;
     bool isGrounded;
     
     Vector2 velocity;
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        velocity.x = context.ReadValue<float>();
+        float speed = context.ReadValue<float>();
+        if (activeAbility == Abilities.SpeedBoost)
+        {
+            speed *= speedMultiplier;
+        }
+        velocity.x = speed;
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (isGrounded)
+        if (isGrounded || (activeAbility == Abilities.DoubleJump && jumpCount < 2))
         {
             velocity.y = jumpHeight;
+            jumpCount++;
         }
     }
 
@@ -41,6 +58,8 @@ public class PlayerMovement : MonoBehaviour
             {
                 velocity.y = -2f;
             }
+
+            jumpCount = 0;
         }
         else
         {
