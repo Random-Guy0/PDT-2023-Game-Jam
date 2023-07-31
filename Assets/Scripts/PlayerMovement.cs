f148e37d109691a225124aa185aeb532a2904d73
Zacosk <62122304+Zacosk@users.noreply.github.com>
2023-07-31 19:49:01 +1000

    Updated player controller and added platform
    - Updated player controller to allow player to pass through platforms while jumping and crouching
    - Fixed triple jump button press/release bug
    - Created platform prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,11 +23,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speedMultiplier = 20f;
 
     [SerializeField] private Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.1f;
     public LayerMask groundMask;
     private int jumpCount = 0;
-    bool isGrounded;
-    
+    bool isGrounded, passThroughPlatform;
+
     Vector2 velocity;
 
     public void OnMove(InputAction.CallbackContext context)
@@ -42,15 +42,31 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (isGrounded || (activeAbility == Abilities.DoubleJump && jumpCount < 2))
+        if (context.performed && (isGrounded || (activeAbility == Abilities.DoubleJump && jumpCount < 1)))
         {
             velocity.y = jumpHeight;
             jumpCount++;
         }
     }
 
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            passThroughPlatform = true;
+        }
+
+        if (context.canceled)
+        {
+            passThroughPlatform = false;
+        }
+    }
+    
+    //public void OnCrouchRelease()
+
     void FixedUpdate()
     {
+        //Check if player is grounded, if true reset jump count and downwards velocity
         if (Physics.CheckSphere(groundCheck.position, groundDistance, groundMask))
         {
             isGrounded = true;
@@ -64,6 +80,16 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             isGrounded = false;
+        }
+        
+        //If player is jumping, allow passing through platforms
+        if ((controller.velocity.y > 0 && !isGrounded) || passThroughPlatform)
+        {
+            Physics.IgnoreLayerCollision(7, 8, true);
+        }
+        else
+        {
+            Physics.IgnoreLayerCollision(7, 8, false);
         }
         
         velocity.y += gravity * Time.deltaTime;
